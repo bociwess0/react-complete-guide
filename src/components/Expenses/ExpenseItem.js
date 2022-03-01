fef4aaec78fabf9947c6b9bc8df5c951b7c5e6f2
bociwess0 <bogdandjordjevic2000@gmail.com>
2022-03-01 19:57:02 +0100

    using useState() to change the title

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,17 +1,35 @@
+import React, { useState } from 'react';
+
 import './ExpenseItem.css';
 import ExpenseDate from "./ExpenseDate";
 import Card from '../UI/Card';
 
 function ExpenseItem(props) {
+    // let title = props.title;
     
+    // const clickHandler = () => {
+    //     title = "Updated!";
+    // }
+    //This is not a solution for this intention
+
+    const [title, setTitle] = useState(props.title);
+
+    // useState always returns an array
+    // where the first value is the variable itself,
+    // you could say, the value itself.
+    // And the second element in the array
+    // is that updating function.
+
     const clickHandler = () => {
-        console.log("Clicked!");
+        setTitle('Updated!');
+        console.log(title);
     }
+
 
     return <Card className='expense-item'>
         <ExpenseDate date={props.date}/>
         <div className='expense-item__description'>
-            <h2>{props.title}</h2>
+            <h2>{title}</h2>
             <div className='expense-item__price'>${props.amount}</div>
         </div>
         <button onClick={clickHandler}>Change Title</button>
