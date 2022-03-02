d0d30faa0ae8a9b2480b0872760c47ed94d535e0
bociwess0 <bogdandjordjevic2000@gmail.com>
2022-03-02 22:13:03 +0100

    communicating bewteen children and parents by using props and events

--- src/components/NewExpense/NewExpense.js
+++ src/components/NewExpense/NewExpense.js
@@ -3,9 +3,18 @@ import React from 'react';
 import ExpenseForm from './ExpenseForm';
 import './NewExpense.css';
 
-const NewExpense = () => {
+const NewExpense = (props) => {
+    const saveExpenseDataHandler = (enteredExpenseData) => {
+        const expenseData = {
+            ...enteredExpenseData, //on alredy stored data, im adding an radnom id
+            id: Math.random().toString()
+        };
+        props.onAddExpense(expenseData);
+    }
+    
+    //pointed edit is passed to ExpenseForm
     return <div className='new-expense'>
-        <ExpenseForm />
+        <ExpenseForm onSaveExpenseData = {saveExpenseDataHandler} />
     </div>
 };
 
