02662cd1eee435ae8f60f4a088936312350635de
bociwess0 <bogdandjordjevic2000@gmail.com>
2022-03-09 21:46:29 +0100

    Form added

---
+++ practice-project-01/src/components/NewUsers.js
@@ -0,0 +1,10 @@
+import React from "react";
+import Form from "./Form";
+
+function NewUsers(props) {
+    return <div className="new-user">
+        <Form />
+    </div>
+}
+
+export default NewUsers;
