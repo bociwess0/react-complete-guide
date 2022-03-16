56ee79499ce74e7e102c49ba8d17248eb8a0a623
bociwess0 <bogdandjordjevic2000@gmail.com>
2022-03-16 20:21:52 +0100

    using UseEffect() for request

--- database-starting-project/src/App.js
+++ database-starting-project/src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 
 import MoviesList from './components/MoviesList';
 import './App.css';
@@ -7,7 +7,9 @@ function App() {
   const [movies, setMovies] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState(null);
-  async function fetchMoviesHandler () {
+
+
+  const fetchMoviesHandler = useCallback (async () => {
     setIsLoading(true);
     setError(null);
     try {
@@ -33,7 +35,12 @@ function App() {
       setError(error.message);
     }
     setIsLoading(false);
-  }
+  }, [])
+
+  useEffect(() => {
+    fetchMoviesHandler();
+  }, [fetchMoviesHandler]);
+
 
   let content = <p>Foung no movies.</p>
 
