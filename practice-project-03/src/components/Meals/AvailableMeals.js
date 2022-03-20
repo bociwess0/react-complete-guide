472affd92d43fa75b8e1997b066c1ab8bf8ec554
bociwess0 <bogdandjordjevic2000@gmail.com>
2022-03-20 21:07:53 +0100

    fetching meals from database

--- practice-project-03/src/components/Meals/AvailableMeals.js
+++ practice-project-03/src/components/Meals/AvailableMeals.js
@@ -1,36 +1,33 @@
 import Card from '../UI/Card';
 import MealItem from './MealItem/MealItem';
 import classes from './AvailableMeals.module.css';
+import { useEffect, useState } from 'react';
 
-const DUMMY_MEALS = [
-  {
-    id: 'm1',
-    name: 'Sushi',
-    description: 'Finest fish and veggies',
-    price: 22.99,
-  },
-  {
-    id: 'm2',
-    name: 'Schnitzel',
-    description: 'A german specialty!',
-    price: 16.5,
-  },
-  {
-    id: 'm3',
-    name: 'Barbecue Burger',
-    description: 'American, raw, meaty',
-    price: 12.99,
-  },
-  {
-    id: 'm4',
-    name: 'Green Bowl',
-    description: 'Healthy...and green...',
-    price: 18.99,
-  },
-];
 
 const AvailableMeals = () => {
-  const mealsList = DUMMY_MEALS.map((meal) => (
+  const [meals, setMeals] = useState([]);
+
+  //we cant use async inside the useEffect() bcs it should not return a promise, it should run synchronisly
+  useEffect(() => {
+    const fetchMeals = async () => { //but we can use async if we create a new function inside the useEffect()
+      const response = await fetch('https://react-http-856b8-default-rtdb.firebaseio.com/meals.json');
+      const responseData = await response.json();
+
+      const loadedMeals = [];
+
+      for(const key in responseData) {
+        loadedMeals.push({
+          id: key,
+          name: responseData[key].name,
+          description: responseData[key].description,
+          price: responseData[key].price
+        });
+      }
+      setMeals(loadedMeals);
+    }
+    fetchMeals();
+  }, [])
+  const mealsList = meals.map((meal) => (
     <MealItem
       key={meal.id}
       id={meal.id}
