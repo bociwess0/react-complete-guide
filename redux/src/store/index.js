18ccfd66ca0e6de2504cec83fc0cd5ee863a1af9
bociwess0 <bogdandjordjevic2000@gmail.com>
2022-04-06 20:37:16 +0200

    using redux toolkit

--- redux/src/store/index.js
+++ redux/src/store/index.js
@@ -1,10 +1,32 @@
 import { createStore } from 'redux';
+import { createSlice } from '@reduxjs/toolkit';
 
 const initialState = {counter : 0, showCounter: true}
+
+createSlice({
+  name: 'counter',
+  initialState: initialState,
+  reducers: {
+    increment(state) {
+      state.counter++; //its allowed here
+    },
+    decrement(state) {
+      state.counter--
+    },
+    increase(state, action) {
+      state.counter += action.amount;
+    },
+    toogleCounter(state) {
+      state.showCounter = !state.showCounter
+    }
+  }
+});
 
 const counterReducer = (state = initialState, action) => {
   if (action.type === 'increment') {
     return {
+    //state.coutner++ NEVER CHANGE THE EXISTING STATE, ALWAYS OVERRIDE IT!
+    //becouse it can have unwanted and unexpected side effects in bigger applications
       counter: state.counter + 1,
       showCoutner: state.showCounter
     };
