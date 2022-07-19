b7ce5d163a2740e8e132d760a953cac25db75133
bociwess0 <bogdandjordjevic2000@gmail.com>
2022-07-19 22:07:41 +0200

    coninuing with redux bases..

--- starting_redux/redux-demo.js
+++ starting_redux/redux-demo.js
@@ -1,9 +1,18 @@
 const redux = require('redux');
 
 const counterReducer = (state = { counter: 0 }, action) => {
+   if(action.type === 'increment') {
+        return {
+            counter: state.counter + 1
+        };
+   }
+
+   if(action.type === 'decrement') {
     return {
-        counter: state.counter + 1
+        counter: state.counter - 1
     };
+}
+   return state;
 }; //creating a reducer
 
 const store = redux.createStore(counterReducer); //creating a store
@@ -17,3 +26,4 @@ const counterSubscriber = () => {
 store.subscribe(counterSubscriber); // executes whenever the data has changed
 
 store.dispatch({ type: 'increment'});
+store.dispatch({ type: 'decrement'});
