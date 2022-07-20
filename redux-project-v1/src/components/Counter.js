3890163782920a6c07942868c4fd070da4e8b0b3
bociwess0 <bogdandjordjevic2000@gmail.com>
2022-07-20 20:32:42 +0200

    dispatching actions from inside of components

--- redux-project-v1/src/components/Counter.js
+++ redux-project-v1/src/components/Counter.js
@@ -1,12 +1,21 @@
 import classes from './Counter.module.css';
-import { useSelector, connect } from 'react-redux'; //that allows us to then automatically select a part of our state managed by the store.
+import { useSelector, useDispatch } from 'react-redux'; //that allows us to then automatically select a part of our state managed by the store.
 
 const Counter = () => {
+  const dispatch = useDispatch();
   const counter = useSelector(state => state.counter);
-/*When you use use selector, React Redux will automatically set up a subscription
-to the Redux store for this component.
-So your component will be updated and will receive the latest counter automatically
-whenever that data changes in the Redux store. */
+  /*When you use use selector, React Redux will automatically set up a subscription
+  to the Redux store for this component.
+  So your component will be updated and will receive the latest counter automatically
+  whenever that data changes in the Redux store. */
+
+  const incrementHandler = () => {
+    dispatch({type: 'increment'});
+  }
+
+  const decrementHandler = () => {
+    dispatch({type: 'decrement'});
+  }
 
   const toggleCounterHandler = () => {};
 
@@ -14,6 +23,10 @@ whenever that data changes in the Redux store. */
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
       <div className={classes.value}>{counter }</div>
+      <div>
+        <button onClick={incrementHandler}>Increment</button>
+        <button onClick={decrementHandler}>Decrement</button>
+      </div>
       <button onClick={toggleCounterHandler}>Toggle Counter</button>
     </main>
   );
--- redux-project-v1/src/store/index.js
+++ redux-project-v1/src/store/index.js
@@ -4,11 +4,15 @@ const redux = require('redux');
 
 const counterReducer = (state = {counter: 0}, action) => {
     if(action.type === 'increment') {
-        return state.counter + 1;
+        return {
+            counter: state.counter + 1
+        }
     }
 
     if(action.type === 'decrement') {
-        return state.counter - 1;
+        return {
+            counter: state.counter - 1
+        }
     }
 
     return state;
