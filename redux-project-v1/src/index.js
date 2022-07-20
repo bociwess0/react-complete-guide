5375b7d4681ad106e9843815cb798e907861283d
bociwess0 <bogdandjordjevic2000@gmail.com>
2022-07-20 20:03:31 +0200

    providing our store to other components in the react app

--- redux-project-v1/src/index.js
+++ redux-project-v1/src/index.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import {Provider} from 'react-redux';
 
 import './index.css';
 import App from './App';
+import store from './store'; 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App />);
+root.render(<Provider store ={store}><App /></Provider>); //providing our store to the react app
+//that means that any child compontent can get data ouf of store and dispatch actions
