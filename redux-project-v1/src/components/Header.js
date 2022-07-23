27c50f2e27a2ee334cc036a488ae6e09c817c97a
bociwess0 <bogdandjordjevic2000@gmail.com>
2022-07-23 13:37:54 +0200

    dispatching login and logout actions

--- redux-project-v1/src/components/Header.js
+++ redux-project-v1/src/components/Header.js
@@ -1,8 +1,15 @@
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import classes from './Header.module.css';
+import { authActions } from '../store';
 
 const Header = () => {
   const isAuth = useSelector(state => state.auth.isAuthentificated);
+  const dispatch = useDispatch();
+
+  const logoutHandler = () => {
+    dispatch(authActions.logout());
+  }
+
   return (
     <header className={classes.header}>
       <h1>Redux Auth</h1>
@@ -15,7 +22,7 @@ const Header = () => {
             <a href='/'>My Sales</a>
           </li>
           <li>
-            <button>Logout</button>
+            <button onClick={logoutHandler}>Logout</button>
           </li>
         </ul>
       </nav>}
