baaa8c5e8f8393bf4bc0a991ee51eaa5cfedbdb2
bociwess0 <bogdandjordjevic2000@gmail.com>
2022-07-26 20:16:23 +0200

    finalizing the fetching logic

--- redux-project-v2/src/store/cart-actions.js
+++ redux-project-v2/src/store/cart-actions.js
@@ -18,7 +18,10 @@ export const fetchCartdata = () => {
 
         try {
             const cartData = await fetchData();
-            dispatch(cartActions.replaceCart(cartData));
+            dispatch(cartActions.replaceCart({
+                items: cartData.items || [],
+                totalQuantity: cartData.totalQuantity
+            }));
         } catch (error) {
             dispatch(
                 uiActions.showNotification({
@@ -47,7 +50,7 @@ export const sendCartData = (cart) => {
           'https://redux-test-c2e28-default-rtdb.firebaseio.com/cart.json',
           {
             method: 'PUT',
-            body: JSON.stringify(cart),
+            body: JSON.stringify({items: cart.items, totalQuantity: cart.totalQuantity}),
           }
         );
   
