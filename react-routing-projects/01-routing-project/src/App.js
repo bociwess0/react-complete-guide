03f365af560b791b105057e1dd7d3e592511a994
bociwess0 <bogdandjordjevic2000@gmail.com>
2022-10-16 15:28:05 +0200

    working with custom hook useParams()

--- react-routing-projects/01-routing-project/src/App.js
+++ react-routing-projects/01-routing-project/src/App.js
@@ -1,7 +1,8 @@
-import { Route } from 'react-router-dom'
+import { Route } from 'react-router-dom';
 
 import Welcome from './pages/Welcome';
 import Products from './pages/Products';
+import ProductDetail from './pages/ProductDetail';
 import MainHeader from './components/MainHeader';
 
 function App() {
@@ -15,9 +16,16 @@ function App() {
         <Route path='/products'>
           <Products />
         </Route>
+        <Route path='/product-detail/:productId'>
+          <ProductDetail />
+        </Route>
       </main>
     </div>
   );
 }
 
 export default App;
+
+// our-domain.com/welcome => Welcome Component
+// our-domain.com/products => Products Component
+// our-domain.com/product-detail/a-book
