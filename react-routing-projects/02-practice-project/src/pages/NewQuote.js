f69cfafd3267de0496c082ab3972254337db2324
bociwess0 <bogdandjordjevic2000@gmail.com>
2022-10-17 20:27:17 +0200

    Implementing Programmatic (Imperative) Navigation

--- react-routing-projects/02-practice-project/src/pages/NewQuote.js
+++ react-routing-projects/02-practice-project/src/pages/NewQuote.js
@@ -1,7 +1,17 @@
+import { useHistory } from 'react-router-dom';
 import QuoteForm from '../components/quotes/QuoteForm';
 
 const NewQuote = () => {
+
+  const history = useHistory();
+
   const addQuoteHandler = (quoteData) => {
+  
+    //storing ad history of browsing into wariable
+
+    history.push('/quotes'); 
+    //history.replace - do not allows user to go back
+
     console.log(quoteData);
   };
 
