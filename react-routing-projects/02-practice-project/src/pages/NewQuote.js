9f162236e41f8bcfe2ae9789342c84621e2aa16d
bociwess0 <bogdandjordjevic2000@gmail.com>
2022-10-20 22:06:47 +0200

    sending an POST Http Request

--- react-routing-projects/02-practice-project/src/pages/NewQuote.js
+++ react-routing-projects/02-practice-project/src/pages/NewQuote.js
@@ -1,21 +1,37 @@
+import { useEffect } from 'react';
 import { useHistory } from 'react-router-dom';
 import QuoteForm from '../components/quotes/QuoteForm';
+import useHttp from '../hooks/use-http';
+import { addQuote } from '../lib/api';
 
 const NewQuote = () => {
 
+  const {sendRequest, status} = useHttp(addQuote);
+  
+
   const history = useHistory();
 
+  useEffect(() => {
+
+    if(status === 'completed') {
+      history.push('/quotes');
+    }
+
+  }, [status, history])
+
+
   const addQuoteHandler = (quoteData) => {
-  
+    
+
+    sendRequest(quoteData);
     //storing ad history of browsing into wariable
 
-    history.push('/quotes'); 
     //history.replace - do not allows user to go back
 
     console.log(quoteData);
   };
 
-  return <QuoteForm onAddQuote={addQuoteHandler} />;
+  return <QuoteForm isLoading = {status === 'pending'} onAddQuote={addQuoteHandler} />;
 };
 
 export default NewQuote;
