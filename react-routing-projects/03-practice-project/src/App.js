d77483cf1a04695bae4240cd28ac06d9b4341770
bociwess0 <bogdandjordjevic2000@gmail.com>
2022-10-29 15:19:18 +0200

    adding routes into App.js file - preparing project

--- react-routing-projects/03-practice-project/src/App.js
+++ react-routing-projects/03-practice-project/src/App.js
@@ -1,9 +1,23 @@
+import { Redirect, Route, Switch } from "react-router-dom";
 import Layout from "./components/layout/Layout";
 
 function App() {
   return (
     <Layout>
-      
+      <Switch>
+        <Route path='/' exact>
+            <Redirect to='/quotes'></Redirect>
+        </Route>
+        <Route path='/quotes' exact>
+
+        </Route>
+        <Route path='/quotes/:quoteId'>
+
+        </Route>
+        <Route path='new-quote'>
+
+        </Route>
+      </Switch>
     </Layout>
   );
 }
