9e62994fe048567aaddc17a4e1acbc11bb93988b
bociwess0 <bogdandjordjevic2000@gmail.com>
2022-11-03 20:08:56 +0100

    successfully sending data to database

---
+++ react-routing-projects/03-practice-project/src/api/api.js
@@ -0,0 +1,30 @@
+const addQuoteFunction = async (quoteData) => {
+  const response = await fetch(
+    'https://react-routing-42d52-default-rtdb.firebaseio.com/quotes.json',
+    {
+      method: 'PUT',
+      body: JSON.stringify({
+        author: quoteData.author,
+        description: quoteData.text,
+      }),
+    }
+  );
+  
+  if (!response.ok) {
+    throw new Error('Sending data failed.');
+  }
+
+  const data = await response.json();
+
+  return data;
+};
+
+export const addQuote = async (quoteData) => {
+  try {
+    await addQuoteFunction(quoteData);
+  } catch(error) {
+    console.log(error);
+  }
+}
+
+  
--- react-routing-projects/03-practice-project/src/components/quotes/QuoteForm.js
+++ react-routing-projects/03-practice-project/src/components/quotes/QuoteForm.js
@@ -1,17 +1,48 @@
-import { Fragment } from 'react';
+import { Fragment, useReducer, useRef } from 'react';
+import { addQuote } from '../../api/api';
+
+
 import classes from './QuoteForm.module.css';
 
 const QuoteForm = (props) => {
+
+    const authorInputRef = useRef();
+    const textInputRef = useRef();
+
+    const addQuoteHandler = (event) => {
+        event.preventDefault();
+
+        const quote = {};
+
+        const enteredAuthor = authorInputRef.current.value;
+        const enteredText = textInputRef.current.value;
+
+        if(enteredAuthor === '') {
+            alert('Plese fill the Author field');
+            return;
+        }
+
+        if(enteredText === '') {
+            alert('Plese fill the Text field');
+            return;
+        }
+
+        quote.author = enteredAuthor;
+        quote.text = enteredText;
+        console.log(quote);
+        addQuote(quote);
+    }
+
   return (
     <Fragment>
-        <form className={classes.form} >
+        <form className={classes.form} onSubmit={addQuoteHandler}>
             <div className={classes.control}>
                 <label htmlFor='author'>Author</label>
-                <input type='text' id='author' />
+                <input type='text' id='author' ref={authorInputRef}/>
             </div>
             <div className={classes.control}>
                 <label htmlFor='text'>Text</label>
-                <textarea id='text' rows='5'></textarea>
+                <textarea id='text' rows='5' ref={textInputRef}></textarea>
             </div>
             <div className={classes.actions}>
                 <button className='btn'>Add Quote</button>
