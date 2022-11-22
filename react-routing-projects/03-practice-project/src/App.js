b0567adec14396a68c1e844020d6def6309573a4
bociwess0 <bogdandjordjevic2000@gmail.com>
2022-11-22 20:45:48 +0100

    adding lazy loading option

--- react-routing-projects/03-practice-project/src/App.js
+++ react-routing-projects/03-practice-project/src/App.js
@@ -1,26 +1,38 @@
+import React from "react";
+import { Suspense } from "react";
 import { Redirect, Route, Switch } from "react-router-dom";
 import Layout from "./components/layout/Layout";
-import AllQuotes from "./pages/AllQuotes";
-import NewQuote from "./pages/NewQuote";
-import QuoteDetail from "./pages/QuoteDetail";
+import LoadingSpinner from "./components/UI/LoadingSinner";
+
+const NewQuote = React.lazy(() => import('./pages/NewQuote'));
+// Now, this function here which we pass to React lazy, will be executed by React, when this new quote component is needed.
+// And that's the key difference.It will not be executed in advance to download the code in advance, but only when it's needed.
+
+const QuoteDetail = React.lazy(() => import('./pages/QuoteDetail'));
+const AllQuotes = React.lazy(() => import('./pages/AllQuotes'));
+
 
 function App() {
   return (
     <Layout>
-      <Switch>
-        <Route path='/' exact>
-            <Redirect to='/quotes'></Redirect>
-        </Route>
-        <Route path='/quotes' exact>
-            <AllQuotes />
-        </Route>
-        <Route path='/quotes/:quoteId'>
-          <QuoteDetail />
-        </Route>
-        <Route path='/new-quote'>
-            <NewQuote />
-        </Route>
-      </Switch>
+      <Suspense fallback={<div className="centered">
+          <LoadingSpinner />
+        </div>} >
+        <Switch>
+          <Route path='/' exact>
+              <Redirect to='/quotes'></Redirect>
+          </Route>
+          <Route path='/quotes' exact>
+              <AllQuotes />
+          </Route>
+          <Route path='/quotes/:quoteId'>
+            <QuoteDetail />
+          </Route>
+          <Route path='/new-quote'>
+              <NewQuote />
+          </Route>
+        </Switch>
+      </Suspense>
     </Layout>
   );
 }
