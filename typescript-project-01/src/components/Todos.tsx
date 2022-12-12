5adf84ed7491ddd6c64c547830d8a5b7fbff5860
bociwess0 <bogdandjordjevic2000@gmail.com>
2022-12-12 20:10:00 +0100

    Adding Styling

--- typescript-project-01/src/components/Todos.tsx
+++ typescript-project-01/src/components/Todos.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import Todo from '../models/todo';
+import classes from './Todos.module.css'
 
 //FC - functional component
 //We're defining here that Todos component will havean items prop
 const Todos: React.FC<{items: Todo[]}> = (props) => {
-    return <ul>
+    return <ul className={classes.todos}>
         {props.items.map((item) => (
-            <li key={item.id}>{item.text}</li>
+            <li className={classes.item} key={item.id}>{item.text}</li>
         ))}
     </ul>
 }
