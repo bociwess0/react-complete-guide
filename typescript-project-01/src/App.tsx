b9d544f09b65fe3dccd6da2c551e487275acb149
bociwess0 <bogdandjordjevic2000@gmail.com>
2022-12-12 20:03:26 +0100

    Managing State & TypeScript

--- typescript-project-01/src/App.tsx
+++ typescript-project-01/src/App.tsx
@@ -1,12 +1,24 @@
+import { useState } from 'react';
 import './App.css';
 import NewTodo from './components/NewTodo';
 import Todos from './components/Todos';
+import Todo from './models/todo';
 
 function App() {
+  //never[] - only an empty array
+  const [todos, setTodos] = useState<Todo[]>([])
+
+  const addTodoHandler = (todoText: string) => {
+    const newTodo = new Todo(todoText);
+    setTodos((prevTodos) => { //udating the previous state
+      return prevTodos.concat(newTodo); //array with elemnts of prevTodos and newTodo
+    })
+  }
+
   return (
     <div className="App">
-      <Todos items={['Learn React', 'Learn Typescript']} />
-      <NewTodo />
+      <NewTodo onAddTodo = {addTodoHandler} />
+      <Todos items={todos} />
     </div>
   );
 }
--- typescript-project-01/src/components/Todos.tsx
+++ typescript-project-01/src/components/Todos.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
+import Todo from '../models/todo';
 
 //FC - functional component
 //We're defining here that Todos component will havean items prop
-const Todos: React.FC<{items: string[]}> = (props) => {
+const Todos: React.FC<{items: Todo[]}> = (props) => {
     return <ul>
         {props.items.map((item) => (
-            <li key={item}>{item}</li>
+            <li key={item.id}>{item.text}</li>
         ))}
     </ul>
 }
